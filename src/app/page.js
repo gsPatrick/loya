19124a02cfe2d0a3d414a43799826b3050794d5f
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-12-01 10:02:18 -0300

    finalizado

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,14 @@
-// src/app/page.js
 "use client";
 
-import Header from "@/componentsLP/Header/Header";
-import Hero from "@/componentsLP/Hero/Hero";
-import HowItWorks from "@/componentsLP/HowItWorks/HowItWorks";
-import Impact from "@/componentsLP/Impact/Impact";
-import Testimonials from "@/componentsLP/Testimonials/Testimonials";
-import FAQ from "@/componentsLP/FAQ/FAQ"; // <--- Novo
-import Footer from "@/componentsLP/Footer/Footer"; // <--- Novo
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LandingPage() {
-    return (
-        <main className="bg-slate-50 min-h-screen w-full overflow-x-hidden selection:bg-teal-500 selection:text-white">
-            <Header />
-            <Hero />
-            <HowItWorks />
-            <Impact />
-            <Testimonials />
-            <FAQ /> {/* <--- Inserido */}
-            <Footer /> {/* <--- Inserido */}
-        </main>
-    );
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("/login");
+    }, [router]);
+
+    return null; // Não renderiza nada
 }
